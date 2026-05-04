77a6f2b3d0cba061a5f16e6fb60f52c7fd6d899c
theWITCHKING8998 <132372692+theWITCHKING8998@users.noreply.github.com>
2026-05-04 14:33:53 +0330

    Delete src/app directory

--- src/app/api/dinosaurs/route.ts
+++
@@ -1,5 +0,0 @@
-import data from "../data.json" with { type: "json" };
-
-export function GET() {
-    return Response.json(data);
-}
--- src/app/types.ts
+++
@@ -1,1 +0,0 @@
-export type Dino = { name: string; description: string };
